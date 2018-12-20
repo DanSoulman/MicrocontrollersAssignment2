cba9f547d8caf6a8510ba62cd4f1fee4e8c15e24
Dan Coleman <32960483+DanSoulman@users.noreply.github.com>
2018-12-20 21:11:31 +0000

    Tickers implemented, code cleaned up,

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-//Whatever the hell Assignment 2 does
-//By Dan Coleman R00151926
+//Temperature and Vibration detection alarm
+//By Dan Coleman R0015192
 
+//Current State: Missing the USB part but I ran out of time to add it. 
 
 #include "mbed.h"
 //Screen and temp Imports----
@@ -10,14 +11,13 @@
 #include "MMA7660.h"
 //---------------------------
 
-C12832 lcd(p5, p7, p6, p8, p11); //Does the Screen bit
+C12832 lcd(p5, p7, p6, p8, p11); //LCD Screen 
 LM75B tmp(p28,p27); //I2C Temperature Sensor
 
-//RGB LED with 3 PWM outputs for dimmer control---
-PwmOut r(p23);
-PwmOut g(p24);
-PwmOut b(p25);
-//------------------------------------------------
+//LEDS-----------------
+DigitalOut redLED(p23);
+PwmOut blueLED(p25);
+//---------------------
 
 //Pins for the accelerometer
 MMA7660 acc(p28, p27);
@@ -26,134 +26,149 @@ MMA7660 acc(p28, p27);
 PwmOut speaker(p26); //Speaker with PWM driver
 Serial pc(USBTX,USBRX); //used for printf to PC over USB
 
-AnalogIn pot1(p19); //Reads Pot 1 - near LCD: This will be used to set the temp threshold 
-AnalogIn pot2(p20); //Reads Pot 2 - near RGB LED: This will be used to set the vibration threshold
+AnalogIn pot1(p19); //Reads Pot 1 - near LCD: This will be used to set the vibration threshold 
+AnalogIn pot2(p20); //Reads Pot 2 - near RGB LED: This will be used to set the temp threshold
+
+//Tickers---------
+Ticker blueSwapper; //Flashes the blueLED
+Ticker redSwapper; //Flashes the redLED
+//----------------
+
+bool attachedBlue = false; //Used to attach blueSwapper 
+
+ //Global Variable declarations-----------------------------
+float defTempThreshold = 30;//default Temp Threshold
+float defDoNotShakeThreshold = 0.5; //default vibration threshold
+float tempThreshold = 0; //Used down below to vary the temperature threshold
+float vibrationThreshold = 0; //Used down below to vary the vibration threshold
+float TotalVibration = 0; //Used to work out the total vibration used 
+//------------------------------------------------------------
+
+//Clears Screen
+void screenClear(){
+     lcd.cls(); //Clears screen          
+     lcd.locate(0,0); //Puts us at pixel location 0,0
+    }
 
 
+//This prints a warning showing what the microcontroller is exceeding 
 void screenWarning(int option){
     switch(option) {
 
     case 1  :
-    lcd.cls(); //Clears screen
-    lcd.locate(0,0); //Puts us at pixel location 0,0
-    lcd.printf("DANGER \nVIBRATION AND \nTEMPERATURE TOO HIGH !!! ");
-    wait(1);
+    screenClear();
+    lcd.printf("VIBRATION & TEMP\nEXCEEDED \nFIX THEN RESET");
+    wait(0.5);
     break;
 
     case 2  :
-    lcd.cls(); //Clears screen
-    lcd.locate(0,0); //Puts us at pixel location 0,0
-    lcd.printf("DANGER \nVIBRATION TOO HIGH");
-    wait(1);
+    screenClear();
+    lcd.printf("DANGER \nEXCEEDED VIBRATION \nTHRESHOLD");
+    wait(0.5);
     break;
-    
-    case 3  :
-    lcd.cls(); //Clears screen
-    lcd.locate(0,0); //Puts us at pixel location 0,0
-    lcd.printf("DANGER \nTEMPERATURE TOO HIGH");
-    wait(1);
-    break;      
-    
-    default  :
-    lcd.cls(); //Clears screen
-    lcd.locate(0,0); //Puts us at pixel location 0,0
-    lcd.printf("Everything is in order");
-    wait(1);
-    break;   
-    
-    }
+  }
 }
 
+//Turns the BlueLED on/off (swapped from previous state) 
+void swapBlueLED(){
+    blueLED = !blueLED;
+}
 
+//Turns the redLED on/off (swapped from previous state
+void swapRedLED(){
+    redLED = !redLED;
+}
 
+//Checks temperature and turns on alarm if needed. 
+void temperatureChecking(float tempThreshold){
+        while(true){
+            //Potentiometer affects the threshold
+            tempThreshold = (defTempThreshold * pot2.read());
 
-
-
-void sensorChecking(float tempThreshold, float vibrationThreshold, float TotalVibration){
-        //If the vibration and Temp are above Thresholds gets more obnoxious
-        if(tmp.read() > tempThreshold && TotalVibration > vibrationThreshold){
-           r = 0.0;
-           g = 1.0;
-           b = 1.0;
-           speaker.period(1/300.0);
-           speaker = 0.5;
-           wait(1);
-           screenWarning(1);  
-           
-        }else if(TotalVibration > vibrationThreshold){
-            b = 0.0;
-            r = 1.0;
-            g = 1.0;
-            speaker.period(1/200.0);
-            speaker = 0.5;
-            wait(1);
-            screenWarning(2);
-           
-        //If the Temperature is above the temp Treshold it flashes red and an alarm is set off
-        }else if(tmp.read() > tempThreshold) {
-            b = 0.0;
-            r = 1.0;
-            g = 1.0;
-            speaker.period(1/200.0);
-            speaker = 0.5;
-            wait(1);
-            screenWarning(3);
+            //If the Temperature is above the temp Treshold it flashes red and an alarm is set off            
+            if(tmp.read() > tempThreshold) {
+                
+                //Swaps from Blue LED to Red
+                blueSwapper.detach();
+                blueLED = 1;
+                redSwapper.attach(&swapRedLED, 0.2);
+                
+                //turns on alarm and warning
+                speaker.period(1/200.0);
+                speaker = 0.5;
+                screenWarning(1);
+            }
             
-        //Turns off light and sound
-        } else { 
-            r = 1.0;
-            g = 0.0;
-            b = 1.0;
-            speaker = 0.0;
-            wait(1);
-        //screenWarning(4); removed for being unnessicary 
-        }   
+        }
+        
+}
+
+//Checks the vibration is above the threshold 
+void vibrationChecking(){
+
+        //Checks if the vibration is above the threshold
+        if(TotalVibration > vibrationThreshold){
+            //Prints Screen warning about the vibration Threshold getting exceeded
+            screenWarning(2);
+            
+            
+            //Flashes blue
+            if(attachedBlue == false){
+                blueSwapper.attach(&swapBlueLED, 1);
+            }
+            
+            //Checks Temperature
+            temperatureChecking(tempThreshold);
+        }
      }
      
 //I made this just to check individual features
 void debugPrint(float xAxis, float yAxis, float zAxis){
-    lcd.cls(); //Clears screen
-    lcd.locate(0,0); //Puts us at pixel location 0,0
+    screenClear();
     //lcd.printf(" xAxis = %.2f\n yAxis = %.2f\n zAxis = %.2f\n " , xAxis, yAxis, zAxis);
     //lcd.printf(" xAxis = %.2f\n yAxis = %.2f\n zAxis = %.2f\n " , -xAxis, -yAxis, -zAxis);
     }
 
 
+
 int main() {
-      //Variable declarations-----------------------------
-        float defTempThreshold = 30;//default Temp Threshold
-        float defDoNotShakeThreshold = 1.5; //default vibration threshold
-        //RGB LED off
-        r=1.0;  g=1.0;  b=1.0;
+       //RGB LED off
+        redLED = 1;
+        blueLED = 1;
         
-      while (1) {// loops consistantly to allow for change if values     
-        //Sets Adjustable Threshold Values---------------------------------------
-        //Varying TempTreshold with Pot 1
-        float tempThreshold = (defTempThreshold * pot1.read());
-        //Varying Vibration Threshold with Pot 2
-        float vibrationThreshold = (defDoNotShakeThreshold * pot2.read());
+        //Axis for vibration
+        float xAxis;
+        float yAxis;
+        float zAxis;
+        
+                
+        while (true) {// loops consistantly to allow for change if values     
+            //Sets Vibration Axis
+            xAxis = acc.x();
+            yAxis = acc.y();
+            zAxis = acc.z();
+            //-------------------
+            
+            //Varying thresholds with potentiometers----------------------------
+            //Varying temperature Threshold with Pot 1
+            tempThreshold = (defTempThreshold * pot2.read());
+            //Varying Vibration Threshold with Pot 1
+            vibrationThreshold = (defDoNotShakeThreshold * pot1.read());
+            //------------------------------------------------------------------
       
       
-       //Reads in the axis for Vibration-----------------------------------------------
-        float xAxis = acc.x();
-        float yAxis = acc.y();
-        float zAxis = acc.z();
-        float TotalVibration = sqrt ((xAxis *xAxis) + (yAxis * yAxis) + (zAxis * zAxis)); //might change this as Nash said the formaulae goes above the max
-      //--------------------------------------------------------------------------------
+            //Reads in the axis for Vibration-----------------------------------------------  
+            TotalVibration = sqrt ((xAxis *xAxis) + (yAxis * yAxis) + (zAxis * zAxis)); //might change this as Nash said the formaulae goes above the max
+            TotalVibration -= 0.98; //This was my (poor) attempt to account for gravity 
+            vibrationChecking();//Checks to see if the vibration exceeds the threshold 
+            //--------------------------------------------------------------------------------
       
-        //Printing the temp to LCD--------------------------
-        lcd.cls(); //Clears screen          TODO: Optimize
-        lcd.locate(0,0); //Puts us at pixel location 0,3
-        lcd.printf("Temp %.2f Vibration %.2f", tmp.read(),TotalVibration); //Prints temp
-        lcd.locate(0, 10); // Set new location for print
-        lcd.printf("Temp Treshold: %.2f", tempThreshold);
-        lcd.locate(0,20);  // Set new location for print
-        lcd.printf("Vibration Treshold: %.2f", vibrationThreshold);
-        //--------------------------------------------------
+            //Printing the temp to LCD--------------------------
+            screenClear();
+            lcd.printf("Temp %.2f Vibration %.2f \nTemp Treshold: %.2f \nVibration Treshold: %.2f" ,tmp.read(),TotalVibration,tempThreshold,vibrationThreshold);
+            wait(0.5);
+            //--------------------------------------------------    
         
-        
-        //debugPrint(xAxis, yAxis, zAxis);
-        sensorChecking(tempThreshold, vibrationThreshold, TotalVibration);
-        wait(0.5);
-     }
-}
+        }//End of while loop    
+
+    }//End of main
